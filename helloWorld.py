0ede7941e0800febb009792e04c48ff6c7d3bb7d
abhijeet36 <37010508+abhijeet36@users.noreply.github.com>
2018-03-03 12:44:14 +0530

    Create helloWorld.py

---
+++ helloWorld.py
@@ -0,0 +1,7 @@
+#@author Abhijeet 
+#@objective Printing Hello World to console
+#This is my first code in Python
+
+print "Hello World"
+name= raw_input("Enter your name:")
+print "You are welcome"+ name
